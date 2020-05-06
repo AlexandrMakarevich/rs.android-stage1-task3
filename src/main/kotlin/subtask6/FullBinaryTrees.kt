e81fa15d73ab5709cb172aa0aadf941d7bb31601
Alex Makarevich <lemas096@gmail.com>
2020-05-06 22:43:28 +0300

    feat: subtasks 1,2,3,5, partOf6

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -4,6 +4,12 @@ class FullBinaryTrees {
 
     // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        val finalString: String = ""
+
+        if (count == 1) return "[[0]]"
+        if (count <= 4) return  "[]"
+
+        //throw NotImplementedError("Not implemented")
+        return finalString
     }
 }
